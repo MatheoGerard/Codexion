6ab723d54256b704c968cccfaa2a51fa64272395
Matheo Gerard <mgerard@shi-c2-p1.s19.be>
2026-08-08 17:31:00 +0200

    patch validate all function to add new parsing

--- srcs/validate.c
+++ srcs/validate.c
@@ -2,5 +2,9 @@
 
 int	validate_all(t_parse_data *data)
 {
-	return (validate_coders_data(data) && validate_timers_data(data) && validate_scheduler(data));
+	return (validate_coders_data(data) 
+			&& validate_timers_data(data) 
+			&& validate_scheduler(data)
+			&& validate_dongle_cooldown(data)
+			&& validate_number_compiles(data));
 }
